efffd650aed9a448308eedc27457588f9f24e0ef
GiacomoCorradini <corra981@gmail.com>
2023-09-20 12:26:06 +0200

    Update plugin

--- src/position_gazebo_plugin.cpp
+++ src/position_gazebo_plugin.cpp
@@ -1,17 +1,3 @@
-// Copyright 2013 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
- 
 #include <ignition/math/Rand.hh>
 #include <gazebo_ros/node.hpp>
 #include <gazebo_ros/utils.hpp>
@@ -21,11 +7,6 @@
 #include <rclcpp/rclcpp.hpp>
 
 #include <position_gazebo_plugin.hpp>
-
-#ifdef NO_ERROR
-// NO_ERROR is a macro defined in Windows that's used as an enum in tf2
-#undef NO_ERROR
-#endif
 
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/transform_broadcaster.h>
